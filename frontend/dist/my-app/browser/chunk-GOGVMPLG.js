bc9b0ba1994401d596ecba641180a099c15660dc
Justin Guo <justing1618@gmail.com>
2024-07-10 20:09:23 -0400

    aws sdk

---
+++ frontend/dist/my-app/browser/chunk-GOGVMPLG.js
@@ -0,0 +1,33 @@
+import{Ba as n,Da as d,R as a,va as e,wa as t,xa as c}from"./chunk-5JZ2RB5H.js";var s=(()=>{let o=class o{};o.\u0275fac=function(r){return new(r||o)},o.\u0275cmp=a({type:o,selectors:[["app-home"]],standalone:!0,features:[d],decls:25,vars:0,consts:[["lang","en"],["charset","UTF-8"],["name","viewport","content","width=device-width, initial-scale=1.0"],["href","#",2,"color","#fff","text-decoration","none"],[1,"container"],["id","projects"],[1,"project"],["href","/wordle","routerLinkActive","active","ariaCurrentWhenActive","page"]],template:function(r,m){r&1&&(e(0,"html",0)(1,"head"),c(2,"meta",1)(3,"meta",2),e(4,"title"),n(5,"My Portfolio - My App"),t()(),e(6,"body")(7,"header")(8,"h1")(9,"a",3),n(10,"Portfolio - My Website"),t()()(),e(11,"div",4)(12,"section",5)(13,"h2"),n(14,"Projects"),t(),e(15,"div",6)(16,"h3"),n(17,"Wordle Solver"),t(),e(18,"p"),n(19,"Given a mystery 5-letter word, the program tries to guess this secret word from being told what letters in each guess are present or are in the correct spot"),t(),e(20,"p"),n(21,"Skills/Technologies used: Python"),t(),e(22,"p")(23,"a",7),n(24,"View Project"),t()()()()()()())},styles:[`body[_ngcontent-%COMP%] {
+            font-family: Arial, sans-serif;
+            line-height: 1.6;
+            background-color: #f0f0f0;
+            padding: 20px;
+            margin: 0;
+        }
+        header[_ngcontent-%COMP%] {
+            background-color: #333;
+            color: #fff;
+            padding: 10px 0;
+            text-align: center;
+            margin-bottom: 20px;
+        }
+        .container[_ngcontent-%COMP%] {
+            max-width: 800px;
+            margin: auto;
+            background-color: #fff;
+            padding: 20px;
+            border-radius: 8px;
+            box-shadow: 0 0 10px rgba(0,0,0,0.1);
+        }
+        .project[_ngcontent-%COMP%] {
+            margin-bottom: 20px;
+            padding-bottom: 20px;
+            border-bottom: 1px solid #ccc;
+        }
+        .project[_ngcontent-%COMP%]   h3[_ngcontent-%COMP%] {
+            margin-top: 0;
+        }
+        .project[_ngcontent-%COMP%]   p[_ngcontent-%COMP%] {
+            margin-bottom: 10px;
+        }`]});let i=o;return i})();export{s as HomeComponent};
